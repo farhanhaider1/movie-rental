5463de8558d9f09fa36d96aeefb4e15337659b51
farhan <fhaider085@gmail.com>
2020-11-11 14:35:06 -0500

    changes to mongodb

--- startup/db.js
+++ startup/db.js
@@ -2,6 +2,6 @@ const winston = require('winston');
 const mongoose = require('mongoose');
 
 module.exports = function(){
-    mongoose.connect('mongodb://localhost/vidly',{ useNewUrlParser: true },{ useUnifiedTopology: true } )
+    mongoose.connect('mongodb+srv://farhantest:1234@testcluster.maheo.mongodb.net/primary?retryWrites=true&w=majority',{ useNewUrlParser: true },{ useUnifiedTopology: true } )
     .then(()=> winston.info('connecting database'));
 };
